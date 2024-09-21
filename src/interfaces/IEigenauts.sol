443fa145127838aa72383a8fb82239b1919d6631
Scott Conner <scottconner@scottsttmachine.attlocal.net>
2024-09-21 09:22:16 -0500

    Going to create a clean interface just for the app functions

---
+++ src/interfaces/IEigenauts.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT 
+pragma solidity ^0.8.23;
+
+/////////////////////////////////////////////////////
+// Eigenauts
+//
+// This ERC721 NFT collection is a private collection that is
+// designed to build community within the Eigen Layer effort, and
+// ultimately, the ecosystem. Eventually this contract will be upgraded
+// out of centralized control into a lightweight self-governing system
+// of Keys - still mutable but governed by the Eigenaut holders.
+//
+// The key and NFT system are designed to enable seemless extensions
+// of the NFT capabilities, so this contract will mainly focus
+// on managing the population of the Eigenaut commmunity, and their
+// governing rites.. Other functions like badge management, game participation,
+// or other extensions to what the Eigenauts are capable of will
+// be delivered as extensible contracts.
+//
+// The main features of the Eigenaut contracts are:
+//
+// 1) Seeding the initial population of Eigenauts for genesis.
+// 2) Setting governance roles for the community.
+// 3) Minting new Eigenauts, and the rules around that.
+// 4) Migrating or moving Eigenauts safely to new wallets.
+//
+/////////////////////////////////////////////////////
+interface Eigenauts {
+ 
+}
